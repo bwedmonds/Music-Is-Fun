b370f9b54248b198d831290b4c1bb265d4a895d3
Bryce Edmonds <bryce@bryceedmonds.com>
2019-05-18 21:25:52 -0600

    working and audio center

--- app/models/Song.js
+++ app/models/Song.js
@@ -12,17 +12,17 @@ export default class Song {
     //draw general layout here
     get Template() {
         return `
-<div class="card mb-3 border-dark" style="max-width: 540px;">
+<div class="card mb-3 border-dark" style="max-width: 500px;">
 <div class="row no-gutters">
 <div class="col-md-4">
-<img src="${this.albumArt}" class="card-img" alt="...">
+<img src="${this.albumArt}" class="card-img" alt="${this.albumArt}">
 </div>
 <div class="col-md-8”>
 <div class="card-body bg-light">
-<h4 class="card-title">${this.artist}</h4>
-<h5 class="card-title">${this.title}</h5>
-<p class="card-text">Buy at iTunes for $${this.price}</p>
-<audio controls>
+<h4 class="card-title text-center">${this.artist}</h4>
+<h5 class="card-title text-center">${this.title}</h5>
+<p class="card-text text-center">Buy at iTunes for $${this.price}</p>
+<audio controls id="audioPlayer">
 <source src="${this.preview}" type="audio/ogg">
 <source src="${this.preview}" type="audio/mpeg">
 Your browser does not support the audio element.
